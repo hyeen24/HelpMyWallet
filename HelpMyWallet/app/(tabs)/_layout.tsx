9df1669a3511e5aef0e0e5c4d826a60ec7c9aad7
Hyeen <81229303+hyeen24@users.noreply.github.com>
2025-04-27 16:05:55 +0800

    Added react-native-svg-transformer

---
+++ HelpMyWallet/app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const Layout = () => {
+  return (
+    <Tabs/>
+  )
+}
+
+export default Layout
