5e1ca68c5bc0190014991b2825a7516f9ee96421
Hyeen <81229303+hyeen24@users.noreply.github.com>
2025-04-27 22:26:06 +0800

    Added Expense Block, static data

--- HelpMyWallet/app/(tabs)/index.tsx
+++ HelpMyWallet/app/(tabs)/index.tsx
@@ -1,35 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { AntDesign, FontAwesome, SimpleLineIcons } from '@expo/vector-icons'
-import Colors from '@/constants/Colors'
-import { Stack } from 'expo-router'
-import Header from '@/components/Header'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { AntDesign, FontAwesome, SimpleLineIcons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { Stack } from "expo-router";
+import Header from "@/components/Header";
+import { PieChart} from "react-native-gifted-charts";
+import ExpenseBlock from "@/components/ExpenseBlock";
+import ExpenseList from '@/data/expenses.json';
 
 const Page = () => {
+    const pieData = [
+        {
+          value: 47,
+          color: '#009FFF',
+          gradientCenterColor: '#006DFF',
+          focused: true,
+        },
+        {value: 40, color: '#93FCF8', gradientCenterColor: '#3BE9DE'},
+        {value: 16, color: '#BDB2FA', gradientCenterColor: '#8F80F3'},
+        {value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97'},
+      ];
   return (
     <>
-    <Stack.Screen options={{
-        header: () => <Header/>
-    }}/>
-        <View style={styles.container}>
-        <Text>Page</Text>
-        </View>
+      <Stack.Screen
+        options={{
+          header: () => <Header />,
+        }}
+      />
+      <View style={[styles.container, { paddingTop: 60 }]}>
+        <ScrollView>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <View style={{ gap: 10 }}>
+                    <Text style={styles.text}>
+                        My 
+                        <Text style={{fontWeight: 700}}> Expenses</Text>
+                    </Text>
+                    <Text style={{ color: Colors.white, fontSize:36, fontWeight: 700}}>$1500.<Text style={{fontSize:22, fontWeight: 400}}>00</Text></Text>
+                </View>
+                
+                <View style={{ paddingVertical: 20, alignItems: "center" }}>
+                    <PieChart
+                        data={pieData}
+                        donut
+                        showGradient
+                        sectionAutoFocus
+                        radius={70}
+                        innerRadius={55}
+                        semiCircle
+                        focusOnPress
+                        innerCircleColor={'#232B5D'}
+                        centerLabelComponent={() => {
+                            return (
+                            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                                <Text
+                                style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}>
+                                47%
+                                </Text>
+                            </View>
+                            );
+                        }}
+                    />
+                </View>
+            </View>
+                <ExpenseBlock expenseList={ExpenseList}/>
+        </ScrollView>
+      </View>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.black,
-        paddingHorizontal: 20
-    },
-    text: {
-        color: Colors.white
-
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    paddingHorizontal: 20,
+  },
+  text: {
+    color: Colors.white,
+  },
+});
---
+++ HelpMyWallet/components/ExpenseBlock.tsx
@@ -0,0 +1,119 @@
+import { FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { ExpenseType } from '@/types';
+import Colors from '@/constants/Colors';
+import { Feather } from '@expo/vector-icons';
+
+const ExpenseBlock = ({expenseList} : {expenseList: ExpenseType[]}) => {
+
+    const totalAmount = expenseList.reduce((sum, expense) => {
+        return sum + parseFloat(expense.amount);
+      }, 0);
+
+    const renderItem: ListRenderItem<Partial<ExpenseType>> = ({item, index}) => {
+        if ( index == 0 ) {
+            return (
+                <TouchableOpacity>
+                    <View style={styles.addCategoryView}>
+                        <Feather name="plus" size={22} color={'#ccc'} />
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+        let amount = item.amount.split('.');
+        let percentage = Math.floor(parseFloat(item.amount) / totalAmount * 100) 
+
+        let BlockColor;
+        let TxtColor;
+
+        switch (item.name) {
+            case "Food" :
+                BlockColor = Colors.blue;
+                TxtColor = Colors.black;
+                break;
+            case "Entertainment" :
+                BlockColor = Colors.tintColor;
+                TxtColor = Colors.white;
+                break;
+            default:
+                BlockColor = Colors.white;
+                TxtColor = Colors.black;
+                break;
+        }
+
+        return(
+            <View style={[styles.expenseBlock,
+                {
+                    backgroundColor: BlockColor
+                }
+            ]}>
+                <Text style={[styles.expenseBlockTitle, { color : TxtColor}]}>{item.name}</Text>
+                <Text style={[styles.expenseAmountWholeNumber, { color : TxtColor}]}>${amount[0]}.
+                    <Text style={[styles.expenseAmountDecimalNumber, { color: TxtColor}]}>{amount[1]}</Text>
+                </Text>
+                <View style={styles.expensePercentageView}>
+                    <Text style={[styles.expenseBlockTitle, { color: TxtColor }]}>{percentage}%</Text>
+                </View>
+            </View>
+        );
+    };
+
+    const staticItem = [{ name: "Add Item"}];
+
+  return (
+    <View>
+      <FlatList 
+        data={staticItem.concat(expenseList)} 
+        renderItem={renderItem} 
+        horizontal 
+        showsHorizontalScrollIndicator={false}/>
+    </View>
+  )
+}
+
+export default ExpenseBlock
+
+const styles = StyleSheet.create({
+    expenseBlock: {
+        backgroundColor: Colors.tintColor,
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        width: 100,
+        padding: 15,
+        borderRadius: 15,
+        marginRight: 20,
+        gap : 8
+    },
+
+    expenseBlockTitle: {
+        fontSize: 14,
+        fontWeight: 600
+    },
+    expenseAmountWholeNumber: {
+        fontSize: 16,
+        fontWeight: 600
+    },
+    expenseAmountDecimalNumber: {
+        fontSize: 12,
+        fontWeight: 400
+    },
+    expensePercentageView : {
+        backgroundColor : 'rgba(255, 255, 255, 0.2)',
+        paddingHorizontal: 5,
+        paddingVertical: 3,
+        borderRadius: 10
+    },
+    addCategoryView : {
+        flex:1,
+        borderWidth: 2,
+        borderColor: '#666',
+        borderStyle: "dashed",
+        borderRadius: 10,
+        marginRight: 20,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+
+})
